17dc7ecaab70744fee36a24b395439a63e94c0a1
BinMouse <63203986+BinMouse@users.noreply.github.com>
2024-11-30 10:10:57 +0200

    Country.sc Correction

---
+++ LibraryDomain/Entities/Country.cs
@@ -0,0 +1,61 @@
+namespace LibraryDomain.Entities;
+
+/// <summary>
+/// Страна
+/// </summary>
+public class Country : BaseEntity
+{
+    /// <summary>
+    /// Конструктор с внутренней инициализацией пустой коллекции препаратов
+    /// </summary>
+    /// <param name="name">Название страны</param>
+    /// <param name="code">Код страны</param>
+    public Country(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+
+    /// <summary>
+    /// Название страны
+    /// </summary>
+    public string Name { get; private set; }
+    
+    /// <summary>
+    /// Код страны
+    /// </summary>
+    public string Code { get; private set; }
+    
+    /// <summary>
+    /// Список препаратов
+    /// </summary>
+    private ICollection<Drug> Drugs { get; } = new List<Drug>();
+
+    /// <summary>
+    /// Добавить препарат в список производимых в стране
+    /// </summary>
+    /// <param name="drug">Препарат</param>
+    /// <exception cref="Exception">Передан пустой объект</exception>
+    public void AddDrug(Drug drug)
+    {
+        if (drug == null) throw new Exception("Drug is null");
+        if (!Drugs.Contains(drug))
+        {
+            Drugs.Add(drug);
+        }
+    }
+    
+    /// <summary>
+    /// Удалить препарат из списка производимых в стране
+    /// </summary>
+    /// <param name="drug">Препарат</param>
+    /// <exception cref="Exception">Передан пустой объект</exception>
+    public void RemoveDrug(Drug drug)
+    {
+        if (drug == null) throw new Exception("Drug is null");
+        if (Drugs.Contains(drug))
+        {
+            Drugs.Remove(drug);
+        }
+    }
+}
